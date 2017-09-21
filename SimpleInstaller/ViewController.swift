784655136cd2360ef66ccb50bcabd99f70f51971
Steve Küng <steve.kueng@srgssr.ch>
2017-09-21 08:56:03 +0200

    Initial Commit

---
+++ SimpleInstaller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleInstaller
+//
+//  Created by Steve Küng on 21.09.17.
+//  Copyright © 2017 Steve Küng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
